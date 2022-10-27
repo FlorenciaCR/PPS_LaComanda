54d0e8a79b418460f916e34c34abdf99920a62d9
maxifernandez1999 <fmaximiliano443@gmail.com>
2022-10-27 19:10:03 -0300

    Creacion de servicio para envio de emails

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: "Los Pollos Hermanos",
+  emailService: {
+    serviceID: "service_acg4g0p",
+    templateID: "template_m4gxh9q"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appName: "Los Pollos Hermanos",
+  emailService: {
+    serviceID: "service_acg4g0p",
+    templateID: "template_m4gxh9q"
+  }
 };
 
 /*
