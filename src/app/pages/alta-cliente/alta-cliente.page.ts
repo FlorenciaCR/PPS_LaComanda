d19b8115ff25aa5b79eab2021f844e5a9ce5b740
FlorenciaCR <florencia.colodro@hotmail.com>
2022-10-28 20:56:25 -0300

    Interfaces

--- src/app/pages/alta-cliente/alta-cliente.page.ts
+++ src/app/pages/alta-cliente/alta-cliente.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Cliente } from 'src/app/interfaces/Cliente';
+
 
 
 @Component({
@@ -12,7 +12,7 @@ import { Cliente } from 'src/app/interfaces/Cliente';
 export class AltaClientePage implements OnInit {
   perfil : string = "anonimo";
   form : FormGroup;
-  cliente : Cliente;
+
   foto : any;
   capturedPhoto : any = "";
   url : any = "";
