6294a1969d1cd613d7580529e62efb6289d5ff78
FlorenciaCR <florencia.colodro@hotmail.com>
2022-11-05 00:33:39 -0300

    Encuesta empleado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,10 +15,7 @@ const routes: Routes = [
   {
     path: 'alta-cliente',
     loadChildren: () => import('./pages/alta-cliente/alta-cliente.module').then( m => m.AltaClientePageModule)
-<<<<<<< HEAD
-=======
 
->>>>>>> Alfa-FlorenciaColodro
   },
   {
     path: 'menu-duenio',
@@ -31,7 +28,7 @@ const routes: Routes = [
   {
     path: 'alta-anonimo',
     loadChildren: () => import('./pages/alta-anonimo/alta-anonimo.module').then( m => m.AltaAnonimoPageModule)
-<<<<<<< HEAD
+
   },
   {
     path: 'home-cliente',
@@ -41,19 +38,33 @@ const routes: Routes = [
     path: 'alta-empleado',
     loadChildren: () => import('./pages/alta-empleado/alta-empleado.module').then( m => m.AltaEmpleadoPageModule)
 
-  },  {
+  },
+  {
     path: 'encuestas',
     loadChildren: () => import('./pages/encuestas/encuestas.module').then( m => m.EncuestasPageModule)
-  }
-
-
-=======
+  },
+  {
+    path: 'pregunta1',
+    loadChildren: () => import('./components/pagesEncuestaEmpleado/pregunta1/pregunta1.module').then( m => m.Pregunta1PageModule)
+  },
+  {
+    path: 'pregunta2',
+    loadChildren: () => import('./components/pagesEncuestaEmpleado/pregunta2/pregunta2.module').then( m => m.Pregunta2PageModule)
+  },
+  {
+    path: 'pregunta3',
+    loadChildren: () => import('./components/pagesEncuestaEmpleado/pregunta3/pregunta3.module').then( m => m.Pregunta3PageModule)
   },  {
-    path: 'pages-encuesta-empleado',
-    loadChildren: () => import('./pages/pages-encuesta-empleado/pages-encuesta-empleado.module').then( m => m.PagesEncuestaEmpleadoPageModule)
+    path: 'pregunta4',
+    loadChildren: () => import('./components/pagesEncuestaEmpleado/pregunta4/pregunta4.module').then( m => m.Pregunta4PageModule)
+  },
+  {
+    path: 'pregunta5',
+    loadChildren: () => import('./components/pagesEncuestaEmpleado/pregunta5/pregunta5.module').then( m => m.Pregunta5PageModule)
   }
 
->>>>>>> Alfa-FlorenciaColodro
+
+
 ];
 
 
